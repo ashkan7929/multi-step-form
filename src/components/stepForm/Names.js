a673eb53f21988211e2957b767be40308017e81c
ashkan ansari <ashkan.ansari.aa@gmail.com>
2021-01-10 19:58:09 +0330

    feat (Names): Create Names component

--- src/components/stepForm/Names.js
+++ src/components/stepForm/Names.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import Container from '@material-ui/core/Container'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
- const Names = () => {
+ const Names = ({formData, setForm, navigation}) => {
+
+    const {firstName, lastName, nickName} = formData;
+    
     return (
-        <div>
-            names
-        </div>
+        <Container maxWidth="xs">
+            <h3>Names</h3>
+            <TextField
+            label = "First Name"
+            name = "firstName"
+            value={firstName}
+            onChange={setForm}
+            margin = "normal"
+            variant = "outlined" autoComplete="off"
+            fullWidth
+            />
+            <TextField
+            label = "Last Name"
+            name = "lastName"
+            value={lastName}
+            onChange = {setForm}
+            margin = "normal"
+            variant = "outlined" autoComplete="off"
+            fullWidth
+            />
+            <TextField
+            label = "NickName"
+            name = "nickName"
+            value={nickName}
+            onChange = {setForm}
+            margin = "normal"
+            variant = "outlined" autoComplete="off"
+            fullWidth
+            />
+            <Button variant="contained" fullWidth color="primary" style={{marginTop : "1rem"}} onClick={()=>navigation.next()}>Next</Button>
+        </Container>
     )
 }
 
