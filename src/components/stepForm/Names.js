2436e425c13128f10e70217cb25c87e816e2c437
ashkan ansari <ashkan.ansari.aa@gmail.com>
2021-01-10 20:01:06 +0330

    Merge branch 'master' of https://github.com/ashkan7929/multi-step-form

--- src/components/stepForm/Names.js
+++ src/components/stepForm/Names.js
@@ -3,43 +3,46 @@ import Container from '@material-ui/core/Container'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 
- const Names = ({formData, setForm, navigation}) => {
-
+const Names = ({formData, setForm, navigation}) => {
+    
     const {firstName, lastName, nickName} = formData;
     
     return (
         <Container maxWidth="xs">
-            <h3>Names</h3>
-            <TextField
-            label = "First Name"
-            name = "firstName"
-            value={firstName}
-            onChange={setForm}
-            margin = "normal"
-            variant = "outlined" autoComplete="off"
-            fullWidth
-            />
-            <TextField
-            label = "Last Name"
-            name = "lastName"
-            value={lastName}
-            onChange = {setForm}
-            margin = "normal"
-            variant = "outlined" autoComplete="off"
-            fullWidth
-            />
-            <TextField
-            label = "NickName"
-            name = "nickName"
-            value={nickName}
-            onChange = {setForm}
-            margin = "normal"
-            variant = "outlined" autoComplete="off"
-            fullWidth
-            />
-            <Button variant="contained" fullWidth color="primary" style={{marginTop : "1rem"}} onClick={()=>navigation.next()}>Next</Button>
+        <h3>Names</h3>
+        <TextField
+        label = "First Name"
+        name = "firstName"
+        value={firstName}
+        onChange={setForm}
+        margin = "normal"
+        variant = "outlined" autoComplete="off"
+        fullWidth
+        />
+        <TextField
+        label = "Last Name"
+        name = "lastName"
+        value={lastName}
+        onChange = {setForm}
+        margin = "normal"
+        variant = "outlined" autoComplete="off"
+        fullWidth
+        />
+        <TextField
+        label = "NickName"
+        name = "nickName"
+        value={nickName}
+        onChange = {setForm}
+        name = "nickname"
+        value={nickName}
+        margin = "normal"
+        variant = "outlined" autoComplete="off"
+        fullWidth
+        />
+        <Button variant="contained" fullWidth color="primary" style={{marginTop : "1rem"}} onClick={()=>navigation.next()}>Next</Button>
         </Container>
-    )
-}
-
-export default Names;
+        )
+    }
+    
+    export default Names;
+    
